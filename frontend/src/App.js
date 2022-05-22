8b284a1bd1264362b462a77c667dd331c50d48ba
Wael <wael.natafji0003@stud.hkr.se>
2022-05-22 13:46:52 +0200

    Clean up project 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Router } from 'react-router-dom';
 import Body from './Components/layout/Body'
 import Home from './views/Home';
 import Profile from './views/Profile';
